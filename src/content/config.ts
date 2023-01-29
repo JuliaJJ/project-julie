362d8d7f53810685a1eff61c9a827cc550845cb5
Julie Johnston <julie.johnston@gmail.com>
2023-01-28 17:37:16 -0800

    update to content schemas

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,43 +1,41 @@
 import { defineCollection, z } from 'astro:content';
 
-const blog = defineCollection({
-	// Type-check frontmatter using a schema
-	schema: z.object({
-		title: z.string(),
-		description: z.string(),
-		// Transform string to Date object
-		pubDate: z
-			.string()
-			.or(z.date())
-			.transform((val) => new Date(val)),
-		updatedDate: z
-			.string()
-			.optional()
-			.transform((str) => (str ? new Date(str) : undefined)),
-		heroImage: z.string().optional(),
+export const collections = { 
+	'blog': defineCollection({
+		// Type-check frontmatter using a schema
+		schema: z.object({
+			title: z.string(),
+			description: z.string(),
+			// Transform string to Date object
+			pubDate: z
+				.string()
+				.or(z.date())
+				.transform((val) => new Date(val)),
+			updatedDate: z
+				.string()
+				.optional()
+				.transform((str) => (str ? new Date(str) : undefined)),
+			heroImage: z.string().optional(),
+		}),
 	}),
-});
-
-const feed = defineCollection({
-	// Type-check frontmatter using a schema
-	schema: z.object({
-		title: z.string(),
-		description: z.string(),
-		image: z.object({
-			src: z.string(),
-			alt: z.string(),
-		  }),
-		// Transform string to Date object
-		pubDate: z
-			.string()
-			.or(z.date())
-			.transform((val) => new Date(val)),
-		updatedDate: z
-			.string()
-			.optional()
-			.transform((str) => (str ? new Date(str) : undefined)),
-		heroImage: z.string().optional(),
+	'feed': defineCollection({
+		// Type-check frontmatter using a schema
+		schema: z.object({
+			title: z.string(),
+			description: z.string(),
+			image: z.object({
+				src: z.string(),
+				alt: z.string(),
+			  }),
+			// Transform string to Date object
+			pubDate: z
+				.string()
+				.or(z.date())
+				.transform((val) => new Date(val)),
+			updatedDate: z
+				.string()
+				.optional()
+				.transform((str) => (str ? new Date(str) : undefined)),
+		}),
 	}),
-});
-
-export const collections = { blog, feed };
+};
